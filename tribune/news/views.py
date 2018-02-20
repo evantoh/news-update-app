b174cc0f463d0a0337ec7e01cf03f90c0a427e09
evantoh <evanmwenda@gmail.com>
2018-02-20 17:12:09 +0300

    display data to the view

--- tribune/news/views.py
+++ tribune/news/views.py
@@ -2,13 +2,15 @@ from django .http import HttpResponse
 import datetime as dt
 from django.http import HttpResponse,Http404
 from django.shortcuts import render,redirect
+from . models import Article 
 #create  your views.
 # def welcome(request):
 #     return render(request,'welcome.html')
 def news_of_day(request):
     date = dt.date.today()
+    news = Article.today_news()
     # function to convert date object to find exact day
-    return render(request,'all_news/today_news.html',{"date":date,})
+    return render(request,'all_news/today_news.html',{"date":date,"news":news})
 
 
 
@@ -26,7 +28,9 @@ def past_days_news(request,past_date):
     if date == dt.date.today():
         return redirect(news_of_day)
 
-    return render (request, 'all_news/past_news.html',{"date":date})
+        news = Article.days_news(date)
+
+    return render (request, 'all_news/past_news.html',{"date":date,"news":news})
 
 
  
