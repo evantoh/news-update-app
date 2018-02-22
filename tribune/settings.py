5cd8a47b241f123a9fe0510db5ab2e0b0bab6d69
evantoh <evanmwenda@gmail.com>
2018-02-22 12:21:33 +0300

    addede more

--- tribune/settings.py
+++ tribune/settings.py
@@ -17,6 +17,7 @@ from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # DATABASES['default'].update(db_from_env) 
 
